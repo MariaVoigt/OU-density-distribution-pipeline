3ae0fe909288aa475064ccee8106aba1065eb90a
Maria Voigt <maria.voigt@idiv.de>
2017-01-13 16:40:59 +0100

    adding optparse logic to abundance submit script

--- src/model_fitting/submit_abundance_model.sh
+++ src/model_fitting/submit_abundance_model.sh
@@ -24,27 +24,13 @@ printf "current git version: %s" $(git rev-parse HEAD)
 [[ -n $(git diff-index --name-only HEAD) ]] && echo "-dirty"
 printf "\n"
 
-if [[ -z $1 ]] ; then
-  echo "qsub $0 /path/to/input"
-  exit 1
-fi
-
-if [[ ! -d $1 ]] ; then
-  echo "directory does not exist: $1"
-  exit 1
-fi
-
-INPUT_PATH=$1
 OUTPUT_PATH=/work/$USER/$JOB_NAME-$JOB_ID
 
-shift
-
-mkdir $OUTPUT_PATH
+mkdir -p $OUTPUT_PATH
 
 export MC_CORES=${NSLOTS:-1}
 
 Rscript \
     abundance_model.R \
-    -i $INPUT_PATH \
     -o $OUTPUT_PATH \
     "$@"
