6ba20b13584366efd3062edd8258966146580fa9
Maria Voigt <maria.voigt@idiv.de>
2017-02-08 13:39:28 +0100

    adding array job ESW

--- src/model_fitting/submit_abundance_model.sh
+++ src/model_fitting/submit_abundance_model.sh
@@ -15,7 +15,6 @@
 
 #$ -m ea
 
-#$ -cwd
 
 module load R
 module load git 2> /dev/null
@@ -26,9 +25,12 @@ printf "\n"
 
 mkdir -p ${OUTPUT_PATH:=/work/$USER/$JOB_NAME-$JOB_ID}
 
+ESW_INDEX=$SGE_TASK_ID
+
 export MC_CORES=${NSLOTS:-1}
 
 Rscript \
     ~/orangutan_density_distribution/src/model_fitting/abundance_model.R \
     -o $OUTPUT_PATH \
+    --ESW-aerial-index $ESW_INDEX
     "$@"
