5c1551d42094f7f2bc2a3055c627c5c43958f1a1
Maria Voigt <maria.voigt@idiv.de>
2017-02-08 18:31:04 +0100

    changing the ESW

--- src/pipeline/submit_pipeline_model.sh
+++ src/pipeline/submit_pipeline_model.sh
@@ -6,7 +6,7 @@
 
 
 # here somehow write the prefix depending on what we are testing
-JOB_NAME_PREFIX=ppln_ae10m_50
+JOB_NAME_PREFIX=ppln_ae100m_50
 
 INPUT_PATH='/work/voigtma/ppln_ae_50_new-2017-02-07T13-46-33'
 NAME=$JOB_NAME_PREFIX-$(date +%FT%H-%M-%S)
@@ -26,7 +26,7 @@ QSUB="qsub -terse -v OUTPUT_PATH=$OUTPUT_PATH -o /work/$USER/$NAME.log -j y"
     -N ${JOB_NAME_PREFIX}_fitting \
     $HOME/orangutan_density_distribution/src/model_fitting/submit_abundance_model.sh \
     -i $INPUT_PATH \
-    --ESW-aerial 0.025 \
+    --ESW-aerial 0.1 \
     --stability \
     --include-aerial)
 
