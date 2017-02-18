8c03edc4160dc8ed11f679cac3b2906ec9043402
Maria Voigt <maria.voigt@idiv.de>
2017-02-18 20:44:43 +0100

    initial commit submit script for bootstrap

---
+++ src/validation/submit_abundance_model_bootstrap.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#$ -N boot
+
+#$ -S /bin/bash
+
+#$ -l h_rt=12:00:00
+#$ -l h_vmem=10G,highmem
+
+#$ -o /work/$USER/$JOB_NAME-$JOB_ID.log
+#$ -j y
+
+#$ -pe smp 10-28
+
+
+#$ -m ea
+
+
+module load R
+module load git 2> /dev/null
+
+printf "current git version: %s" $(git rev-parse HEAD)
+[[ -n $(git diff-index --name-only HEAD) ]] && echo "-dirty"
+printf "\n"
+
+mkdir -p ${OUTPUT_PATH:=/work/$USER/$JOB_NAME-$JOB_ID}
+
+export MC_CORES=${NSLOTS:-1}
+
+Rscript \
+    ~/orangutan_density_distribution/src/validation/abundance_model_bootstrap.R \
+    -o $OUTPUT_PATH \
+    "$@"
