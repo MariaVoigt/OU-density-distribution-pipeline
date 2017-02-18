9f4cbb33687eaeefec7d5e387d91d0c8d3874640
Maria Voigt <maria.voigt@idiv.de>
2017-02-18 12:19:13 +0100

    changing folder and names

--- src/pipeline/submit_pipeline_model.sh
+++ src/pipeline/submit_pipeline_model.sh
@@ -6,12 +6,12 @@
 
 
 # here somehow write the prefix depending on what we are testing
-JOB_NAME_PREFIX=ppln_noa_50_ac
+JOB_NAME_PREFIX=ppln_ae75m_50_ac
 
-INPUT_PATH='/work/voigtma/ppln_noae_50_ac-2017-02-09T18-31-21'
+INPUT_PATH='/work/voigtma/ppln_ae75m_50_ac-2017-02-15T14-23-58'
 NAME=$JOB_NAME_PREFIX-$(date +%FT%H-%M-%S)
 
-OUTPUT_PATH=/work/$USER/$NAME
+OUTPUT_PATH=$INPUT_PATH
 
 # qsub -v means that OUTPUT_PATH goes into environment (normally export, but with qsub different)
 QSUB="qsub -terse -v OUTPUT_PATH=$OUTPUT_PATH -o /work/$USER/$NAME.log -j y"
@@ -27,6 +27,7 @@ QSUB="qsub -terse -v OUTPUT_PATH=$OUTPUT_PATH -o /work/$USER/$NAME.log -j y"
     $HOME/orangutan_density_distribution/src/model_fitting/submit_abundance_model_ac_term.sh \
     -i $INPUT_PATH \
     --ESW-aerial 0.075 \
+    --include-aerial \
     --stability)
 
 
@@ -36,6 +37,5 @@ QSUB="qsub -terse -v OUTPUT_PATH=$OUTPUT_PATH -o /work/$USER/$NAME.log -j y"
     -hold_jid $MODEL_JOB_ID_FITTING \
     -t 1999:2015 \
     $HOME/orangutan_density_distribution/src/prediction/submit_abundance_pred.sh \
-    -i $OUTPUT_PATH \
-    -q)
+    -i $OUTPUT_PATH )
 
