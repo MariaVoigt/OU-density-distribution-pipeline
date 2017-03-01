6bf574121586d04e01086bce8195b35b9c3361d3
Maria Voigt <maria.voigt@idiv.de>
2017-03-01 18:28:06 +0100

    changing outdir and submit to ac-model

--- src/pipeline/submit_pipeline_model.sh
+++ src/pipeline/submit_pipeline_model.sh
@@ -6,12 +6,13 @@
 
 
 # here somehow write the prefix depending on what we are testing
-JOB_NAME_PREFIX=ppln_ae75m_50
+JOB_NAME_PREFIX=ppln_ae75m_50_ac
 
-INPUT_PATH='/work/voigtma/ppln_ae75m_50-2017-02-14T16-39-11'
+INPUT_PATH='/work/voigtma/ppln_ae75m_50_fixing-2017-02-28T18-00-52'
 NAME=$JOB_NAME_PREFIX-$(date +%FT%H-%M-%S)
 
-OUTPUT_PATH=$INPUT_PATH
+# FIX THIS
+OUTPUT_PATH=/work/$USER/$NAME
 
 # qsub -v means that OUTPUT_PATH goes into environment (normally export, but with qsub different)
 QSUB="qsub -terse -v OUTPUT_PATH=$OUTPUT_PATH -o /work/$USER/$NAME.log -j y"
@@ -24,7 +25,7 @@ QSUB="qsub -terse -v OUTPUT_PATH=$OUTPUT_PATH -o /work/$USER/$NAME.log -j y"
 
   MODEL_JOB_ID_FITTING=$($QSUB  \
     -N ${JOB_NAME_PREFIX}_fitting \
-    $HOME/orangutan_density_distribution/src/model_fitting/submit_abundance_model.sh \
+    $HOME/orangutan_density_distribution/src/model_fitting/submit_abundance_model_ac_term.sh \
     -i $INPUT_PATH \
     --ESW-aerial 0.075 \
     --include-aerial \
